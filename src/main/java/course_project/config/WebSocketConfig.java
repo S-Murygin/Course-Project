7a97f7c022df46fba8fc045cabf1c5ca84c498ef
SMurygin <murygin-sr@mail.ru>
2021-09-26 21:16:02 +0300

    Создание конфигуратора WebSocket
    Задан адрес "/web-socket" для подключения клиентов.

---
+++ src/main/java/course_project/config/WebSocketConfig.java
@@ -0,0 +1,17 @@
+package course_project.config;
+
+import course_project.handler.ChatTextWebSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
+        webSocketHandlerRegistry.addHandler(new ChatTextWebSocketHandler(), "/web-socket");
+    }
+}
