4cefa24e57f4e08e01e959945d7d70c5b2b8b92a
SMurygin <murygin-sr@mail.ru>
2021-09-30 22:46:50 +0300

    Добавление в контроллер поддержки STOMP

--- src/main/java/course_project/controller/WebSocketController.java
+++ src/main/java/course_project/controller/WebSocketController.java
@@ -1,13 +1,22 @@
 package course_project.controller;
 
+import course_project.dto.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebSocketController {
 
-    @RequestMapping("/websocket") // контролер
+    @GetMapping("/chat")
     public String getWebSocket() {
-        return "ws-broadcast";
+        return "chat";
+    }
+
+    @MessageMapping("/broadcast")
+    @SendTo("/topic/broadcast")
+    public ChatMessage send(ChatMessage chatMessage) {
+        return new ChatMessage(chatMessage.getFrom(), chatMessage.getText(), "ALL");
     }
 }
