8f6af73cf71347254d2db4c855e088745218ac43
SMurygin <murygin-sr@mail.ru>
2021-10-04 22:34:20 +0300

    Добавление поддержки STOMP в js

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,44 +1,83 @@
-let ws;
+let stompClient = null;
+let userName = $("#from").val();
 
 function setConnected(connected) {
+	$("#from").prop("disabled", connected);
 	$("#connect").prop("disabled", connected);
 	$("#disconnect").prop("disabled", !connected);
+
 	if (connected) {
 		$("#sendmessage").show();
 	} else {
-	    $("#sendmessage").hide();
+		$("#sendmessage").hide();
 	}
 }
 
 function connect() {
-	const url = 'ws://localhost:8080/web-socket';
-	ws = new WebSocket(url);
-	ws.onopen = function() {
-    	showBroadcastMessage('<div class="alert alert-success">Подключились к серверу</div>');
-	};
-	ws.onmessage = function(data) {
-		showBroadcastMessage(createTextNode(data.data));
-	};
-	setConnected(true);
+	userName = $("#from").val();
+	if (userName == null || userName === "") {
+		alert('Пожалуйста введите псевдоним!');
+		return;
+	}
+
+	const url = 'ws://localhost:8080/broadcast';
+
+	stompClient = Stomp.client(url);
+	stompClient.connect({}, function() {
+		stompClient.subscribe('/topic/broadcast', function(output) {
+			showBroadcastMessage(createTextNode(JSON.parse(output.body)));
+		});
+
+		sendConnection(' подключился к серверу');
+		setConnected(true);
+	}, function(err) {
+		alert('error: ' + err);
+	});
 }
 
 function disconnect() {
-	if (ws.readyState === WebSocket.OPEN) {
-		ws.close();
-		showBroadcastMessage('<div class="alert alert-warning">Отключились от сервера</div>');
+	if (stompClient != null) {
+		sendConnection(' отключился от сервера');
+
+		stompClient.disconnect(function() {
+			console.log('отключение...');
+			setConnected(false);
+		})
 	}
-	setConnected(false);
+}
+
+function sendConnection(message) {
+	const text = userName + message;
+	sendBroadcast({'from': 'Сервер', 'text': text});
+}
+
+function sendBroadcast(json) {
+	stompClient.send("/app/broadcast", {}, JSON.stringify(json));
 }
 
 function send() {
-	ws.send($("#message").val());
+	const text = $("#message").val();
+	sendBroadcast({'from': userName, 'text': text});
 	$("#message").val("");
 }
 
-function createTextNode(message) {
-	return '<div class="alert alert-info">' + message + '</div>';
+function createTextNode(messageObj) {
+	return '<div class="row alert alert-info"><div class="col-md-8">' +
+			messageObj.text +
+			'</div><div class="col-md-4 text-right"><small>[<b>' +
+			messageObj.from +
+			'</b> ' +
+			messageObj.time +
+			']</small>' +
+			'</div></div>';
 }
 
 function showBroadcastMessage(message) {
 	$("#content").html($("#content").html() + message);
+	$("#clear").show();
+}
+
+function clearBroadcast() {
+	$("#content").html("");
+	$("#clear").hide();
 }
