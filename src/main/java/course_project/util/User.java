67f5eefbc161d48381dece1326b9462f091c8d33
SMurygin <murygin-sr@mail.ru>
2021-10-15 22:35:28 +0300

    Реализация интерфейса Principal
    Клас возвращает имя пользователя осуществившего подключение.

---
+++ src/main/java/course_project/util/User.java
@@ -0,0 +1,17 @@
+package course_project.util;
+
+import java.security.Principal;
+
+public class User implements Principal {
+
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
