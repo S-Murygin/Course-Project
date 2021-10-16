e752e1756c5eaf3196304f1175081c65a9fc32d4
SMurygin <murygin-sr@mail.ru>
2021-10-16 15:19:15 +0300

    Реализация интерфейса ChannelInterceptor
    Переопределен метод preSend для извлечения имени пользователя из заголовка при подключении. Для дальнейшей идентификации канала подключения данного пользователя.

---
+++ src/main/java/course_project/util/UserInterceptor.java
@@ -0,0 +1,29 @@
+package course_project.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+
+@Slf4j
+public class UserInterceptor implements ChannelInterceptor {
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+
+        StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+        if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+            String name = accessor.getFirstNativeHeader("username");
+
+            accessor.setUser(new User(name));
+
+            log.info("Имя подключившегося пользователя: " + name);
+        }
+
+        return message;
+    }
+}
