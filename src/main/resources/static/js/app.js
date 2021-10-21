b5b6d3b9f784a24cae42f5201ad15edad5247c4d
SMurygin <murygin-sr@mail.ru>
2021-10-21 22:55:00 +0300

    Обновление app.js
    Реализация поддержки функционала чата со стороны пользовательского интерфейса.

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,4 +1,5 @@
 let stompClient = null;
+let selectedUser = null;
 let userName = $("#from").val();
 
 function setConnected(connected) {
@@ -7,77 +8,202 @@ function setConnected(connected) {
 	$("#disconnect").prop("disabled", !connected);
 
 	if (connected) {
+		$("#users").show();
 		$("#sendmessage").show();
 	} else {
+		$("#users").hide();
 		$("#sendmessage").hide();
+
+		setSelectedUser("");
 	}
 }
 
 function connect() {
 	userName = $("#from").val();
+
 	if (userName == null || userName === "") {
 		alert('Пожалуйста введите псевдоним!');
 		return;
 	}
 
-	const socket = new SockJS('/broadcast');
+	$.post('/rest/user-connect', {username: userName},
+        function() {
+			const socket = new SockJS('/chatSocket');
 
-	stompClient = Stomp.over(socket);
-	stompClient.connect({}, function() {
-		stompClient.subscribe('/topic/broadcast', function(output) {
-			showBroadcastMessage(createTextNode(JSON.parse(output.body)));
-		});
+			stompClient = Stomp.over(socket);
+			stompClient.connect({username: userName}, function() {
+			    console.log('connected...');
 
-		sendConnection(' подключился к серверу');
-		setConnected(true);
-	}, function(err) {
-		alert('error: ' + err);
+				stompClient.subscribe('/topic/broadcast', function(output) {
+					showMessage(createTextNode(JSON.parse(output.body)));
+				});
+
+				stompClient.subscribe('/topic/active', function(output) {
+                    updateActiveUsers(userName, JSON.parse(output.body))
+				});
+
+				stompClient.subscribe('/user/queue/messages', function(output) {
+					showMessage(createTextNode(JSON.parse(output.body)));
+				});
+
+				sendConnection(' подключился к серверу');
+				updateUsers(userName);
+				setConnected(true);
+
+			}, function(err) {
+				alert('Error ' + err);
+			});
+
+	}).done(function() {
+//		alert('Request done!');
+
+	}).fail(function(jqxhr, settings, ex) {
+		console.log('Failed, ' + ex);
 	});
 }
 
 function disconnect() {
 	if (stompClient != null) {
-		sendConnection(' отключился от сервера');
+		$.post('/rest/user-disconnect', {username: userName},
+			function() {
+				sendConnection(' отключился от сервера');
 
-		stompClient.disconnect(function() {
-			console.log('отключение...');
-			setConnected(false);
-		})
+				stompClient.disconnect(function() {
+					console.log('disconnected...');
+					setConnected(false);
+				});
+
+		}).done(function() {
+//			alert('Request done!');
+
+		}).fail(function(jqxhr, settings, ex) {
+			console.log('Failed, ' + ex);
+		});
 	}
 }
 
 function sendConnection(message) {
 	const text = userName + message;
-	sendBroadcast({'from': 'Сервер', 'text': text});
+	sendBroadcast({'from': 'server', 'text': text});
 }
 
 function sendBroadcast(json) {
-	stompClient.send("/app/broadcast", {}, JSON.stringify(json));
+	stompClient.send("/chatApp/broadcast", {}, JSON.stringify(json));
 }
 
 function send() {
 	const text = $("#message").val();
-	sendBroadcast({'from': userName, 'text': text});
+
+	if (selectedUser == null) {
+		alert('Пожалуйста выберите пользователя.');
+		return;
+	}
+
+	stompClient.send("/chatApp/chat", {'sender': userName},
+		JSON.stringify({'from': userName, 'text': text, 'recipient': selectedUser}));
 	$("#message").val("");
 }
 
 function createTextNode(messageObj) {
-	return '<div class="row alert alert-info"><div class="col-md-8">' +
+	let classAlert = "alert-info";
+	let fromTo = messageObj.from;
+	let addTo = fromTo;
+
+	if (userName === messageObj.from) {
+		fromTo = messageObj.recipient;
+		addTo = '<i class="fas fa-angle-right"></i> ' + fromTo;
+	}
+
+	if (userName !== messageObj.from && messageObj.from !== "server") {
+		classAlert = "alert-warning";
+	}
+
+	if (messageObj.from !== "server") {
+		addTo = '<a href="javascript:void(0)" onclick="setSelectedUser(\'' + fromTo + '\')">' + addTo + '</a>';
+	}
+
+	return '<div class="row alert ' + classAlert + '"><div class="col-md-8">' +
 			messageObj.text +
 			'</div><div class="col-md-4 text-right"><small>[<b>' +
-			messageObj.from +
+			addTo +
 			'</b> ' +
 			messageObj.time +
 			']</small>' +
 			'</div></div>';
 }
 
-function showBroadcastMessage(message) {
+function showMessage(message) {
 	$("#content").html($("#content").html() + message);
 	$("#clear").show();
 }
 
-function clearBroadcast() {
+function clearMessages() {
 	$("#content").html("");
 	$("#clear").hide();
+}
+
+function setSelectedUser(username) {
+	selectedUser = username;
+	$("#selectedUser").html(selectedUser);
+
+	if (selectedUser === "") {
+	    $("#divSelectedUser").hide();
+	}
+	else {
+	    $("#divSelectedUser").show();
+	}
+}
+
+function updateActiveUsers(username, userList) {
+	const activeUserSpan = $("#active-users-span");
+	const activeUserUL = $("#active-users");
+
+	activeUserUL.html('');
+
+	if (userList.length === 1) {
+        activeUserSpan.html('<p><i>Активные пользователи не найдены.</i></p>')
+    } else {
+    	activeUserSpan.html('<p class="text-muted">Выберите пользователя чтобы начать беседовать.</p>');
+
+    	for (let index = 0; index < userList.length; index++) {
+    	    if (userList[index] !== username) {
+    		    activeUserUL.html(activeUserUL.html() + createUserNode(userList[index], index));
+    		}
+    	}
+    }
+}
+
+function updateUsers(userName) {
+	const activeUserSpan = $("#active-users-span");
+	const activeUserUL = $("#active-users");
+
+	activeUserUL.html('');
+
+	const url = "/rest/active-users-except/" + userName;
+
+	$.ajax({
+		type: 'GET',
+		url: url,
+		dataType: 'json',
+		success: function(userList) {
+			if (userList.length === 0) {
+				activeUserSpan.html('<p><i>Активные пользователи не найдены.</i></p>')
+			} else {
+				activeUserSpan.html('<p class="text-muted">Выберите пользователя чтобы начать беседовать.</p>');
+
+				for (let index = 0; index < userList.length; index++) {
+					activeUserUL.html(activeUserUL.html() + createUserNode(userList[index], index));
+				}
+			}
+		},
+		error: function(XMLHttpRequest, textStatus, errorThrown) {
+			alert("error occurred");
+		}
+	});
+}
+
+function createUserNode(username, index) {
+	return  '<li class="list-group-item">' +
+			'<a class="active-user" href="javascript:void(0)" onclick="setSelectedUser(\'' + username + '\')">' + username + '</a>' +
+			'</li>';
 }
