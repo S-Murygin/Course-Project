3ff1bf17b1ddbd9f1d378de267f4c908216b7d5e
SMurygin <murygin-sr@mail.ru>
2021-11-15 22:27:32 +0300

    Добавление настроек безопасности

---
+++ src/main/java/course_project/config/WebSecurityConfig.java
@@ -0,0 +1,48 @@
+package course_project.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.
+             authorizeRequests()
+                .antMatchers("/h2/**").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and()
+             .formLogin()
+                .defaultSuccessUrl("/chat-ui", true)
+                .and()
+             .logout()
+                .and()
+             .csrf().ignoringAntMatchers("/h2/**")
+                .and()
+             .headers().frameOptions().sameOrigin();
+    }
+}
